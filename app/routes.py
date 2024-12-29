22c4a63be0bc4d1be9889bad933916acf9253943
Melusi Makola <101884620+MelusiMakola@users.noreply.github.com>
2024-12-29 13:11:23 +0200

    Added routes, templates, and static files

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,11 @@ main = Blueprint('main', __name__)
 @main.route("/")
 def home():
     return render_template("index.html")
+
+@main.route("/about")
+def about():
+    return render_template("about.html")
+
+@main.route("/contact")
+def contact():
+    return render_template("contact.html")
